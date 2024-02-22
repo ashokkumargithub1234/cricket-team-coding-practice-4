4f33de9be75be806d1818596fcc1c6c268c1ed09
ashokkumargithub1234 <ashokkumarpenjuvvi@gmail.com>
2024-02-22 08:36:38 +0000

    first commit

--- app.js
+++ app.js
@@ -1,118 +1,9 @@
-/* const express = require("express");
-const { open } = require("sqlite");
-const sqlite3 = require("sqlite3");
-const path = require("path");
-const cors = require("cors");
-
-const databasePath = path.join(__dirname, "cricketTeam.db");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-let database = null;
-
-const initializeDbAndServer = async () => {
-  try {
-    database = await open({
-      filename: databasePath,
-      driver: sqlite3.Database,
-    });
-    app.listen(3000, () =>
-      console.log("Server Running at http://localhost:3000/")
-    );
-  } catch (error) {
-    console.log(`DB Error: ${error.message}`);
-    process.exit(1);
-  }
-};
-
-initializeDbAndServer();
-
-const convertDbObjectToResponseObject = (dbObject) => {
-  return {
-    playerId: dbObject.player_id,
-    playerName: dbObject.player_name,
-    jerseyNumber: dbObject.jersey_number,
-    role: dbObject.role,
-  };
-};
-
-app.get("/players/", async (request, response) => {
-  const getPlayersQuery = `
-    SELECT
-      *
-    FROM
-      cricket_team;`;
-  const playersArray = await database.all(getPlayersQuery);
-  response.send(
-    playersArray.map((eachPlayer) =>
-      convertDbObjectToResponseObject(eachPlayer)
-    )
-  );
-});
-
-app.get("/players/:playerId/", async (request, response) => {
-  const { playerId } = request.params;
-  const getPlayerQuery = `
-    SELECT 
-      * 
-    FROM 
-      cricket_team 
-    WHERE 
-      player_id = ${playerId};`;
-  const player = await database.get(getPlayerQuery);
-  response.send(convertDbObjectToResponseObject(player));
-});
-
-app.post("/players/", async (request, response) => {
-  const { playerName, jerseyNumber, role } = request.body;
-  const postPlayerQuery = `
-  INSERT INTO
-    cricket_team (player_name, jersey_number, role)
-  VALUES
-    ('${playerName}', ${jerseyNumber}, '${role}');`;
-  const player = await database.run(postPlayerQuery);
-  console.log(player);
-  response.send("Player Added to Team");
-});
-
-app.put("/players/:playerId/", async (request, response) => {
-  const { playerName, jerseyNumber, role } = request.body;
-  const { playerId } = request.params;
-  const updatePlayerQuery = `
-  UPDATE
-    cricket_team
-  SET
-    player_name = '${playerName}',
-    jersey_number = ${jerseyNumber},
-    role = '${role}'
-  WHERE
-    player_id = ${playerId};`;
-
-  await database.run(updatePlayerQuery);
-  response.send("Player Details Updated");
-});
-
-app.delete("/players/:playerId/", async (request, response) => {
-  const { playerId } = request.params;
-  const deletePlayerQuery = `
-  DELETE FROM
-    cricket_team
-  WHERE
-    player_id = ${playerId};`;
-  await database.run(deletePlayerQuery);
-  response.send("Player Removed");
-});
-module.exports = app;
-*/
-
 const express = require("express");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 const format = require("date-fns/format");
-// const axios = require("axios");
+const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
@@ -155,7 +46,7 @@ const createTable = async () => {
 
   await database.run(createQuery);
 };
-/*
+
 const getInitializedDatabase = async () => {
   const url = "https://s3.amazonaws.com/roxiler.com/product_transaction.json";
   const responseData = await axios.get(url);
@@ -183,7 +74,7 @@ app.get("/initialize-database", async (request, response) => {
   const initializedDatabase = await getInitializedDatabase();
   response.send(initializedDatabase);
 });
-*/
+
 /* -----------------------CRUD OPERATIONS------------------------------*/
 
 app.get("/get-product/:productId", async (request, response) => {
@@ -468,7 +359,7 @@ app.get("/combined-response", async (request, response) => {
     productId = 0,
   } = request.query;
   const combinedData = {
-    // initialization: await getInitializedDatabase(),
+    initialization: await getInitializedDatabase(),
     transactions: await getAllTransactions(
       searchText,
       selectedMonth,
